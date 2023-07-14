f746e95d6318e04d8cedb898c42997516a386f97
John Kim <johnkim@Johns-MacBook-Air.local>
2023-07-13 22:05:04 -0700

    1.9 unicafe exercise complete

--- src/App.js
+++ src/App.js
@@ -1,31 +1,56 @@
-import './App.css';
-
-const Hello = (props) => {
-    const bornYear = () => {
-        const yearNow = new Date().getFullYear();
-        return yearNow - props.age;
-    }
-
-    return (
-        <div>
-            <p>
-                Hello {props.name}, you are {props.age} years old
-            </p>
-        </div>
-    )
-}
+import { useState } from 'react'
+import {findAllByDisplayValue} from "@testing-library/react";
 
 const App = () => {
-    const name = 'Peter';
-    const age = 10;
+    // save clicks of each button to its own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+
     return (
-        <div>
-            <h1>Greetings</h1>
-            <Hello name={name} age={17 + 22}></Hello>
-            <Hello name='Jajaan' age={age}></Hello>
-            <Hello></Hello>
-        </div>
+        <>
+            <div>
+                <div>
+                    <h1>give feedback</h1>
+                </div>
+                <div>
+                    <button onClick={() => setGood(good + 1)}>Good</button>
+                    <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+                    <button onClick={() => setBad(bad + 1)}>Bad</button>
+                </div>
+                <Statistics good={good} neutral={neutral} bad={bad}/>
+            </div>
+        </>
     )
 }
 
-export default App;
+const Statistics = ({good, neutral, bad}) => {
+    let all = good + neutral + bad;
+    let average = (good * 1 + bad * -1 + neutral * 0) / all;
+    let positive = good / all;
+    if (all === 0) {
+        return (
+            <div>
+                <p>No feedback given</p>
+            </div>
+        )
+    } else {
+        return (
+            <>
+                <div>
+                    <h1>statistics</h1>
+                </div>
+                <div>good {good}</div>
+                <div>neutral {neutral}</div>
+                <div>bad {bad}</div>
+                <div>all {all}</div>
+                <div>average {average ? average : 0}</div>
+                <div>positive {positive ? positive : 0}</div>
+            </>
+        )
+    }
+
+}
+
+export default App
