f8a5bd2a18f88f35a7416261f0409185562621a6
John Kim <johnkim@Johns-MacBook-Air.local>
2023-07-13 18:04:45 -0700

    first setup commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Hello = (props) => {
+    const bornYear = () => {
+        const yearNow = new Date().getFullYear();
+        return yearNow - props.age;
+    }
+
+    return (
+        <div>
+            <p>
+                Hello {props.name}, you are {props.age} years old
+            </p>
+        </div>
+    )
+}
+
+const App = () => {
+    const name = 'Peter';
+    const age = 10;
+    return (
+        <div>
+            <h1>Greetings</h1>
+            <Hello name={name} age={17 + 22}></Hello>
+            <Hello name='Jajaan' age={age}></Hello>
+            <Hello></Hello>
+        </div>
+    )
 }
 
 export default App;
